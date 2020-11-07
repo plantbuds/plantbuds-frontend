93c321d0d39e65be9875e6270cad8cd32f08b23d
jsungg <justinsungg@gmail.com>
2020-11-07 12:44:03 -0800

    added boilerplate for push notifications

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,29 +1,38 @@
 import React, { useEffect } from "react";
-import {
-    StyleSheet,
-    Text,
-    View,
-  } from "react-native";
+import { StyleSheet, Text, View, Vibration } from "react-native";
+import { Notifications } from "expo";
+import { registerForPushNotificationsAsync } from "../utils/Notifications";
 
-  interface Props {
-    navigation: any;
+interface Props {
+  navigation: any;
+}
+
+export default function LandingScreen(props: Props) {
+  const { navigation } = props;
+
+  useEffect(() => {
+    // Get the expo token that identifies this device for notifs
+    registerForPushNotificationsAsync();
+
+    // Vibrate when receiving incoming notifications
+    Notifications.addListener(notif => {
+      Vibration.vibrate();
+      console.log(notif);
+    });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Landing page for PlantBuds</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   }
-
-  export default function LandingScreen(props: Props) {
-    const { navigation } = props;
-
-    return (
-      <View style={styles.container}>
-        <Text>Landing page for PlantBuds</Text>
-      </View>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center"
-    }
-  });
+});
