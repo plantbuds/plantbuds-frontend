8e1fc982c0504377d466ff7a8d8a071037c60c52
vhon0603 <41207326+vhon0603@users.noreply.github.com>
2020-11-17 15:10:00 -0800

    super super simple profile screen cause my branch is fcked up

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Vibration } from "react-native";
+import { StyleSheet, Text, View, Vibration, Button, Image } from "react-native";
 
 // declare types for your props here
 interface Props {
@@ -11,7 +11,24 @@ export default function ProfileScreen(props: Props) {
 
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.profilePicture}
+        source={{
+          uri: 'https://media1.fdncms.com/illinoistimes/imager/u/original/11623518/news01.jpg',
+        }}
+      />
       <Text>Profile!</Text>
+      <Text>Name</Text>
+      <Text>USDA Zone</Text>
+      <Text>Notifications</Text>
+      <Text>Watering Reminder</Text>
+      <Text>Repotting Reminder</Text>
+      <Text>Fertilizing Reminder</Text>
+
+      <Text>Settings</Text>
+      <Text>Notification Delivery</Text>
+      <Button title="Edit" onPress={() => navigation.openDrawer()} />
+
     </View>
   );
 }
@@ -22,5 +39,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
+
+  profilePicture : {
+    flexDirection: 'column',
+    padding: '10',
+    borderColor: 'black',
+    width: 150,
+    height: 150,
+    borderRadius: 100
+  },
 });
