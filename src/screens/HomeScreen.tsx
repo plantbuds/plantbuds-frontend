36f5ed65a1e8d0ca1f8082c9a04ee43dfb77db33
kevatoloco <47438509+kevatoloco@users.noreply.github.com>
2020-11-17 05:26:55 -0800

    skeleton of the skeleton for homepage (#11)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,7 +3,8 @@ import * as Notifications from "expo-notifications";
 import { registerForPushNotificationsAsync } from "../utils/Notifications";
 import { removeAllNotificationListeners } from "expo-notifications";
 import { useIsDrawerOpen } from "@react-navigation/drawer";
-import { Button, StyleSheet, Text, View, Vibration } from "react-native";
+import { Button, StyleSheet, Text, View, Vibration, Image } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 // declare types for your props here
 interface Props {
@@ -37,7 +38,33 @@ export default function HomeScreen(props: Props) {
 
   return (
     <View style={styles.container}>
-      <Text>Home!</Text>
+
+      <View style={styles.searchBar}>
+        <TextInput 
+          //clearTextOnFocus = true
+          defaultValue = "Search my plants"
+        />
+      </View>
+    
+      <View style={styles.fixToText}>
+        <Button title="Sort" disabled onPress={() => navigation.openDrawer()}/>
+        <Button title="Filter" disabled onPress={() => navigation.openDrawer()}/>
+      </View>
+
+      <Image
+        style={styles.plantPicture}
+        source={{
+          uri: 'https://media1.fdncms.com/illinoistimes/imager/u/original/11623518/news01.jpg',
+        }}
+      />
+
+      <Image
+        style={styles.plantPicture}
+        source={{
+          uri: 'https://cse.ucsd.edu/sites/cse.ucsd.edu/files/faculty/gillespie17M-115x150.jpg',
+        }}
+      />
+      
       <Button title="Sidebar" onPress={() => navigation.openDrawer()} />
     </View>
   );
@@ -47,7 +74,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+    alignItems: "flex-start",
+    //justifyContent: "center"
+  },
+
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  searchBar: {
+    borderColor: 'grey', 
+    borderWidth: 2, 
+  },
+
+  plantPicture: {
+    flexDirection: 'row',
+    padding: '10',
+    borderColor: 'black',
+    width: 143,
+    height: 170,
+  },
 });
