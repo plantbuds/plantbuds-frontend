df14f285d031c7298e9ec95bc155d7e76050ce09
jjs050 <jjs050@ucsd.edu>
2020-11-18 20:27:04 -0800

    added boilerplate for redux-native-paper

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,11 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      ['babel-preset-expo'],
-      '@babel/preset-typescript',
-    ],
+    presets: [["babel-preset-expo"], "@babel/preset-typescript"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"]
+      }
+    }
   };
 };
