584ab107bedd3094b69c405a28ca94a3d4929bf9
Justin Sung <47491122+jsungg@users.noreply.github.com>
2020-11-20 13:20:48 -0800

    Boilerplate for react-native-paper provider + Edit Profile Screen (#16)

--- App.tsx
+++ App.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
+import { Provider as PaperProvider } from "react-native-paper";
 import "react-native-gesture-handler";
-import configureStore from './store/store';
+import configureStore from "./store/store";
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigator from "./src/navigation/AppStack";
 
-export const store = configureStore(); 
+export const store = configureStore();
 
 export default function App() {
   return (
-    <Provider store ={store}>
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <StatusBar barStyle="dark-content" />
-        <RootNavigator />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <SafeAreaView style={styles.container}>
+            <StatusBar barStyle="dark-content" />
+            <RootNavigator />
+          </SafeAreaView>
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   );
 }
@@ -24,6 +27,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-  },
+    backgroundColor: "#fff"
+  }
 });
