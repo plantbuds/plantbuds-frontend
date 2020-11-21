3cd46e7def5099ddd8a2e52798d9023f4e4fba60
jsungg <justinsungg@gmail.com>
2020-11-20 23:30:23 -0800

    finished array rendering and styling of homepage

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,9 +2,22 @@ import React, { useEffect, useRef, useState } from "react";
 import * as Notifications from "expo-notifications";
 import { registerForPushNotificationsAsync } from "../utils/Notifications";
 import { removeAllNotificationListeners } from "expo-notifications";
-import { useIsDrawerOpen } from "@react-navigation/drawer";
-import { Button, StyleSheet, Text, View, Vibration, Image } from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { Searchbar } from "react-native-paper";
+import {
+  Button,
+  StyleSheet,
+  Platform,
+  Text,
+  FlatList,
+  View,
+  Dimensions,
+  Vibration,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+  KeyboardAvoidingView
+} from "react-native";
 
 // declare types for your props here
 interface Props {
@@ -19,80 +32,162 @@ Notifications.setNotificationHandler({
   })
 });
 
-export default function HomeScreen(props: Props) {
-  useEffect(() => {
-    // Get the expo token that identifies this device for notifs
-    registerForPushNotificationsAsync();
+const theme = {
+  colors: {
+    placeholder: "#BEBEBE",
+    text: "#959695"
+  }
+};
 
-    // Vibrate when receiving incoming notifications
-    notificationListener.current = Notifications.addNotificationReceivedListener(
-      notif => {
-        Vibration.vibrate();
-        console.log(notif);
-      }
-    );
+export default function HomeScreen(props: Props) {
+  const [loading, setLoading] = useState(true);
+  const [plants, setPlant] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query: string) => setSearchQuery(query);
+
+  useEffect(() => {
+    (async () => {
+      // Get the expo token that identifies this device for notifs
+      registerForPushNotificationsAsync();
+
+      // Vibrate when receiving incoming notifications
+      notificationListener.current = Notifications.addNotificationReceivedListener(
+        notif => {
+          Vibration.vibrate();
+          console.log(notif);
+        }
+      );
+
+      const url = "https://api.randomuser.me/?results=40";
+      const response = await fetch(url);
+      const data = await response.json();
+      setPlant(data.results);
+      setLoading(false);
+    })();
   }, []);
 
   const { navigation } = props;
   const notificationListener = useRef(null);
 
+  if (loading) {
+    return (
+      <View>
+        <Text>loading...</Text>
+      </View>
+    );
+  }
+
+  if (!plants) {
+    return (
+      <View>
+        <Text>didn't get a plant</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.container}>
+          <Searchbar
+            theme={theme}
+            inputStyle={styles.searchBarInput}
+            style={styles.searchBar}
+            placeholder="Search my plants"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
 
-      <View style={styles.searchBar}>
-        <TextInput 
-          //clearTextOnFocus = true
-          defaultValue = "Search my plants"
-        />
-      </View>
-    
-      <View style={styles.fixToText}>
-        <Button title="Sort" disabled onPress={() => navigation.openDrawer()}/>
-        <Button title="Filter" disabled onPress={() => navigation.openDrawer()}/>
-      </View>
+          <View style={styles.fixToText}>
+            <Button
+              title="Sort"
+              disabled
+              onPress={() => navigation.openDrawer()}
+            />
+            <Button
+              title="Filter"
+              disabled
+              onPress={() => navigation.openDrawer()}
+            />
+          </View>
 
-      <Image
-        style={styles.plantPicture}
-        source={{
-          uri: 'https://media1.fdncms.com/illinoistimes/imager/u/original/11623518/news01.jpg',
-        }}
-      />
-
-      <Image
-        style={styles.plantPicture}
-        source={{
-          uri: 'https://cse.ucsd.edu/sites/cse.ucsd.edu/files/faculty/gillespie17M-115x150.jpg',
-        }}
-      />
-      
-      <Button title="Sidebar" onPress={() => navigation.openDrawer()} />
-    </View>
+          <FlatList
+            numColumns={2}
+            columnWrapperStyle={styles.displayWrapper}
+            keyExtractor={item => item.login.uuid}
+            data={plants}
+            renderItem={({ item }) => (
+              <TouchableHighlight
+                key={item.key}
+                activeOpacity={0.6}
+                underlayColor="#DDDDDD"
+                onPress={() => alert("Pressed!")}
+              >
+                <View>
+                  <Image
+                    style={styles.item}
+                    source={{ uri: item.picture.large }}
+                  />
+                  <Text style={{ alignSelf: "center" }}>{item.name.first}</Text>
+                </View>
+              </TouchableHighlight>
+            )}
+          />
+          <Button title="Sidebar" onPress={() => navigation.openDrawer()} />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "flex-start",
-    //justifyContent: "center"
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
 
   searchBar: {
-    borderColor: 'grey', 
-    borderWidth: 2, 
+    borderColor: "grey",
+    backgroundColor: "#F2F2F2",
+    borderWidth: 2,
+    borderRadius: 50,
+    marginTop: 20,
+    width: windowWidth * 0.8
   },
-
+  searchBarInput: {
+    backgroundColor: "#F2F2F2"
+  },
   plantPicture: {
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 10,
-    borderColor: 'black',
+    borderColor: "black",
     width: 143,
-    height: 170,
+    height: 170
   },
+  item: {
+    width: 143,
+    height: 142.93,
+    borderRadius: 30
+  },
+  displayWrapper: {
+    width: windowWidth * 0.95,
+    height: windowHeight * 0.3,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around"
+  }
 });
