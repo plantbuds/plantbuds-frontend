950cffaf3a2026fc4e4c8f5341553a6c7af5af3f
valhon0603 <41207326+valhon0603@users.noreply.github.com>
2020-11-28 21:57:35 -0800

    Vh/pb 48 plant profile screen (#27)
    * styling for plant profile screen
    
    * miscellenous consistency tings
    
    * sum more styling AHHHHH
    
    * pr changes - not sure if I covered them all
    
    * oops added a change YIKE
    
    * :((( i pray dis works
    
    * plz let this b the last change lajs;lfj;lfj
    
    * wed night before rebase
    
    * trying to fix merge conflicts
    
    * wed night before rebase
    
    * trying to fix merge conflicts
    
    * last one i swear
    
    * another one bc i'm dumb b energy
    
    Co-authored-by: vhon0603 <41207326+vhon0603@users.noreply.github.com>
    Co-authored-by: Justin Sung <47491122+jsungg@users.noreply.github.com>

--- src/screens/PlantProfileScreen.tsx
+++ src/screens/PlantProfileScreen.tsx
@@ -1,6 +1,21 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
-import { Button } from "react-native-paper";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  PixelRatio,
+  Switch
+} from "react-native";
+import { Button, TextInput } from "react-native-paper";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import { CardStyleInterpolators } from "@react-navigation/stack";
 import SetReminderModal from "../components/SetReminderModal";
 import AddEntryModal from "../components/AddEntryModal";
 
@@ -21,18 +36,81 @@ export default function PlantProfileScreen(props: Props) {
 
   return (
     <View style={styles.container}>
-      <Button onPress={() => navigation.navigate("EditPlantProfile")}>
-        Edit
-      </Button>
+      <View style={{flexDirection:'row', alignSelf:'center'}}>
+        <Button
+          labelStyle={styles.buttonStyle}
+          onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.homeButtonStyle}>Plant Home</Text>
+        </Button>
+        <Text style={styles.textTitle}></Text>
+        <Button 
+          labelStyle = {styles.buttonStyle}
+          onPress={() => navigation.navigate("EditPlantProfile")}>
+          <Text style={styles.editButtonStyle}>Edit</Text>
+        </Button>
+      </View>
+
+      <View style ={{flexDirection: 'row'}}>
+        <View style = {{flexDirection: 'column'}}>
+          <View style = {styles.containerPicture}>
+            <Image
+              style={styles.profilePicture}
+              source={{
+                uri: itemURI,
+              }}
+            />
+          </View>
+        </View>
+        <View style = {{flexDirection: 'column'}}>
+          <Text style = {styles.plantNameStyle}>{itemName}</Text>
+          <Text style = {styles.scientificZoneStyle}>Scientific Name</Text>
+          <Text style = {styles.scientificZoneStyle}>Zone: #</Text>
+        </View>
+      </View>
+
+      <View>
+      <Text style={styles.NRTParentStyle}> Notes </Text>
+      <Text style = {styles.randomStyling}> Placeholder for notes! </Text> 
+      </View>
+
+      <View>
+      <Text style={styles.NRTParentStyle}> Reminders </Text>
+      </View>
+      
+      <View style={styles.smallerPhoneStyling}>
+        <Text style={styles.NRTChildStyle}>Watering Reminder</Text>
+        <Text style={styles.NRTChildStyle}>Last Watered Date</Text>
+      </View>
+      <View style={styles.smallerPhoneStyling}>
+        <Text style={styles.NRTChildStyle}>Repotting Reminder</Text>
+        <Text style={styles.NRTChildStyle}>Last Repotted Date</Text>
+      </View>
+      <View style={styles.smallerPhoneStyling}>
+        <Text style={styles.NRTChildStyle}>Fertilizing Reminder</Text>
+        <Text style={styles.NRTChildStyle}>Last Fertilized Date</Text>
+      </View>
+      
+
+      <View>
+      <Text style={styles.NRTParentStyle}> Task History </Text>
+      </View>
+
+      <View style={styles.smallerPhoneStyling}>
+        <Text style={styles.NRTChildStyle}>Watered</Text>
+        <Text style={styles.NRTChildStyle}>Date Placeholder</Text>
+      </View>
+      <View style={styles.smallerPhoneStyling}>
+        <Text style={styles.NRTChildStyle}>Repotted</Text>
+        <Text style={styles.NRTChildStyle}>Date Placeholder</Text>
+      </View>
+      <View style={styles.smallerPhoneStyling}>
+        <Text style={styles.NRTChildStyle}>Fertilized</Text>
+        <Text style={styles.NRTChildStyle}>Date Placeholder</Text>
+      </View>
+
+
       <View style={{ flexDirection: "row" }}>
         <View style={{ flexDirection: "column" }}>
-          <Image
-            style={styles.profilePicture}
-            source={{
-              uri: itemURI
-            }}
-          />
-          <Text>{itemName}</Text>
           <Button onPress={() => setDisplayReminderModal(true)}>
             Add start date for reminder (calendar + # of days modal)
           </Button>
@@ -59,23 +137,117 @@ export default function PlantProfileScreen(props: Props) {
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   container: {
     display: "flex",
     flex: 1,
     backgroundColor: "#fff"
+  },
 
+  buttonStyle: {
+    borderRadius: 1,
+    fontSize: 18,
+    textTransform: 'none',
+    color:'#64A3A3',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
   },
-  button: {
-    alignItems: "flex-end",
-    backgroundColor: "lightblue",
-    borderRadius: 1
+
+  columnStyle:{
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%'
   },
+
+  containerPicture: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    paddingTop: 10,
+    height: windowHeight * 0.23,
+    marginLeft: 20
+  },
+
+  editButtonStyle:{
+    alignItems: 'flex-end',
+  },
+
+  homeButtonStyle:{
+    alignItems: 'flex-start',
+  },
+
   profilePicture: {
     flexDirection: "column",
     borderColor: "black",
     width: 150,
     height: 150,
     borderRadius: 100
+  },
+    
+  NRTChildStyle:{
+    fontStyle: "normal",
+    fontWeight: "normal", 
+    color: "#666666",
+    fontSize: 16,
+    lineHeight:30
+  },
+
+  NRTParentStyle:{
+    marginLeft: 15, 
+    fontStyle: "normal",
+    fontWeight: "normal", 
+    color: "#000",
+    fontSize: 22,
+    lineHeight:30,
+    justifyContent:'space-around'
+  },
+
+  plantNameStyle: {
+    alignContent:'center',
+    top:'30%',
+    fontSize: 26, 
+    marginLeft: 10,
+    fontStyle: "normal",
+    fontWeight: "normal",
+    color: "#000",
+    paddingBottom: 3
+  },
+
+  randomStyling: {
+    fontStyle: "normal",
+    fontWeight: "normal", 
+    color: "#666666",
+    fontSize: 16,
+    lineHeight:30,
+    marginLeft: 40
+  },
+
+  scientificZoneStyle:{
+    alignContent:'center',
+    top:'30%',
+    fontSize: 16, 
+    marginLeft: 10,
+    fontStyle: "normal",
+    fontWeight: "normal",
+    color: "#666666",
+    paddingBottom: 3
+  },
+
+  smallerPhoneStyling:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignSelf: "center",
+    width: windowWidth * 0.78
+
+  },
+
+  textTitle:{
+    fontSize: 24,
+    color: "#000000",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    width: '50%'
   }
+
 });
