79f6e5729b69988389ef74ea4ade83dc3da8f2f5
Shauna Sapper <38957509+shaunasapper@users.noreply.github.com>
2020-12-07 10:58:41 -0800

    last minute profile styling to match the rest of the app

--- src/screens/PlantProfileScreen.tsx
+++ src/screens/PlantProfileScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Platform,
   StyleSheet,
@@ -12,17 +12,17 @@ import {
   PixelRatio,
   Switch,
 } from 'react-native';
-import {Text, Colors, IconButton, Button, FAB} from 'react-native-paper';
-import {HeaderBackButton} from '@react-navigation/stack';
-import {updateTaskHistory, setEditedEntry, resetPlantState, setEditedNotif} from '../../store/plantgroup/actions';
-import {RootState} from '../../store/store';
-import {useDispatch, useSelector} from 'react-redux';
+import { Text, Colors, IconButton, Button, FAB, Headline, Subheading, Title, Paragraph, Caption, Divider } from 'react-native-paper';
+import { HeaderBackButton } from '@react-navigation/stack';
+import { updateTaskHistory, setEditedEntry, resetPlantState, setEditedNotif } from '../../store/plantgroup/actions';
+import { RootState } from '../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
 import SetWaterReminderModal from '../components/SetWaterReminderModal';
 import SetRepotReminderModal from '../components/SetRepotReminderModal';
 import SetFertilizeReminderModal from '../components/SetFertilizeReminderModal';
 import AddEntryModal from '../components/AddEntryModal';
-import {Calendar as ReactCalendar} from 'react-native-calendars';
-import {State} from 'react-native-gesture-handler';
+import { Calendar as ReactCalendar } from 'react-native-calendars';
+import { State } from 'react-native-gesture-handler';
 
 // declare types for your props here
 interface Props {
@@ -111,7 +111,7 @@ const greenBrown = {
 };
 
 export default function PlantProfileScreen(props: Props) {
-  const {navigation, route} = props;
+  const { navigation, route } = props;
   const [selectedDate, setSelectedDate] = useState(new Date().toDateString());
   const [markings, setMarkings] = useState({});
   const [waterStatus, setWaterStatus] = useState(false);
@@ -256,135 +256,144 @@ export default function PlantProfileScreen(props: Props) {
   return (
     <View style={styles.container}>
       <ScrollView>
-        <View style={{flexDirection: 'row'}}>
-          <View style={{flexDirection: 'column'}}>
-            <View style={styles.containerPicture}>
-              <Image
-                style={styles.profilePicture}
-                source={{
-                  uri: photo,
-                }}
-              />
-            </View>
+
+        <View style={{ marginLeft: 20, flexDirection: 'row' }}>
+          <View style={styles.containerPicture}>
+            <Image
+              style={styles.profilePicture}
+              source={{ uri: photo }}
+            />
           </View>
-          <View style={{flexDirection: 'column'}}>
-            <Text style={styles.plantNameStyle}>
-              {nickname
-                ? nickname.length > 13
+          <View style={{marginLeft: 10, justifyContent: 'center', flexDirection: 'column'}}>
+            <Headline>
+              {nickname ? nickname
+                /*? nickname.length > 13
                   ? nickname.substring(0, 12) + '...'
-                  : nickname
+                  : nickname*/
                 : 'My Plant'}
-            </Text>
-            <Text style={styles.scientificZoneStyle}>
-              {plant_name
-                ? plant_name.length > 20
+            </Headline>
+            <Title>
+              {plant_name ? plant_name
+                /*? plant_name.length > 20
                   ? plant_name.substring(0, 19) + '...'
-                  : plant_name
-                : 'Scientific Plant Name'}
-            </Text>
+                  : plant_name*/
+                : 'Species'}
+            </Title>
           </View>
         </View>
 
-        <View style={{paddingTop: 10}}>
-          <Text style={styles.NRTParentStyle}> Notes </Text>
-          <Text style={styles.randomStyling}>{notes ? notes : 'No notes yet'}</Text>
-        </View>
+        <View style={{}}>
+          <View style={{ paddingTop: 10 }}>
+            <Title style={styles.NRTParentStyle}> Notes </Title>
+            <Paragraph style={styles.randomStyling}>{notes ? notes : 'No notes yet'}</Paragraph>
+          </View>
 
-        <View>
-          <Text style={styles.NRTParentStyle}>Scheduled Reminders</Text>
-        </View>
+          <View style={{paddingTop: 10}}>
+            <Title style={styles.NRTParentStyle}>Scheduled Reminders</Title>
+          </View>
 
-        <View style={styles.smallerPhoneStyling}>
-          <Text style={styles.NRTChildStyle}>Water</Text>
-          <Text>
-            {water_history && water_history.length > 0 ? water_history[0] : 'No Reminders'}
-          </Text>
-          <IconButton
-            icon="pencil"
-            style={styles.editButton}
-            onPress={() => setDisplayWaterModal(true)}
-          />
-        </View>
+          <View style={styles.row}>
+            <Subheading style={styles.NRTChildStyle}>Water</Subheading>
+            <Paragraph>
+              {water_history && water_history.length > 0 ? water_history[0] : 'No Reminders'}
+            </Paragraph>
+            <IconButton
+              icon="pencil"
+              //style={styles.editButton}
+              style={{alignSelf: 'flex-end'}}
+              onPress={() => setDisplayWaterModal(true)}
+            />
+          </View>
+          <Divider />
 
-        <View style={styles.smallerPhoneStyling}>
-          <Text style={styles.NRTChildStyle}>Repot</Text>
-          <Text>
-            {repot_history && repot_history.length > 0 ? repot_history[0] : 'No Reminders'}
-          </Text>
-          <IconButton
-            icon="pencil"
-            style={styles.editButton}
-            onPress={() => setDisplayRepotModal(true)}
-          />
-        </View>
+          <View style={styles.row}>
+            <Subheading style={styles.NRTChildStyle}>Repot</Subheading>
+            <Paragraph>
+              {repot_history && repot_history.length > 0 ? repot_history[0] : 'No Reminders'}
+            </Paragraph>
+            <IconButton
+              icon="pencil"
+              //style={styles.editButton}
+              style={{alignSelf: 'flex-end'}}
+              onPress={() => setDisplayRepotModal(true)}
+            />
+          </View>
+          <Divider />
 
-        <View style={styles.smallerPhoneStyling}>
-          <Text style={styles.NRTChildStyle}>Fertilize</Text>
-          <Text style={styles.fertilizeText}>
-            {fertilize_history && fertilize_history.length > 0
-              ? fertilize_history[0]
-              : 'No Reminders'}
-          </Text>
-          <IconButton
-            icon="pencil"
-            style={styles.editButton}
-            onPress={() => setDisplayFertilizeModal(true)}
-          />
-        </View>
+          <View style={styles.row}>
+            <Subheading style={styles.NRTChildStyle}>Fertilize</Subheading>
+            <Paragraph>
+              {fertilize_history && fertilize_history.length > 0
+                ? fertilize_history[0]
+                : 'No Reminders'}
+            </Paragraph>
+            <IconButton
+              icon="pencil"
+              //style={styles.editButton}
+              style={{alignSelf: 'flex-end'}}
+              onPress={() => setDisplayFertilizeModal(true)}
+            />
+          </View>
 
-        <View style={{paddingTop: 10}}>
-          <Text style={styles.NRTParentStyle}> Task History </Text>
-          <ReactCalendar
-            enableSwipeMonths={true}
-            onDayPress={day => {
-              setSelectedDate(day.dateString);
-              if (entries[selectedDate] != null) {
-                setWaterStatus(entries[selectedDate][0]);
-                setRepotStatus(entries[selectedDate][1]);
-                setFertilizeStatus(entries[selectedDate][2]);
-              } else {
-                setWaterStatus(false);
-                setRepotStatus(false);
-                setFertilizeStatus(false);
-              }
-              updateCalendarMarkings();
-            }}
-            markedDates={markings}
-            maxDate={new Date()}
-            markingType={'custom'}
-            theme={{
-              backgroundColor: '#ffffff',
-              calendarBackground: '#ffffff',
-              textSectionTitleColor: Colors.grey600,
-              selectedDayTextColor: 'white',
-              selectedDayBackgroundColor: Colors.lightGreen300,
-              arrowColor: Colors.lightGreen300,
-              todayTextColor: Colors.lightGreen300,
-              dayTextColor: Colors.grey600,
-              textDisabledColor: Colors.grey400,
-            }}
-          />
-          <Button onPress={() => setDisplayAddEntryModal(true)}>Edit Entry</Button>
-          <AddEntryModal
-            selectedDate={selectedDate}
-            displayModal={displayAddEntryModal}
-            onPress={() => setDisplayAddEntryModal(false)}
-            onExit={() => setDisplayAddEntryModal(false)}
-            entries={entries}
-            updateCalendarMarkings={() => updateCalendarMarkings}
-          />
-          <SetWaterReminderModal
-            displayModal={displayWaterModal}
-            onExit={() => setDisplayWaterModal(false)}
-          />
-          <SetRepotReminderModal
-            displayModal={displayRepotModal}
-            onExit={() => setDisplayRepotModal(false)}
-          />
-          <SetFertilizeReminderModal
-            displayModal={displayFertilizeModal}
-            onExit={() => setDisplayFertilizeModal(false)}
-          />
+          <View style={{ paddingTop: 10 }}>
+            <Title style={styles.NRTParentStyle}> Task History </Title>
+            <Paragraph style={styles.randomStyling}>
+              Select a date, then tap and hold to edit the care entries for that date.
+            </Paragraph>
+            <ReactCalendar
+              enableSwipeMonths={true}
+              onDayPress={day => {
+                setSelectedDate(day.dateString);
+                if (entries[selectedDate] != null) {
+                  setWaterStatus(entries[selectedDate][0]);
+                  setRepotStatus(entries[selectedDate][1]);
+                  setFertilizeStatus(entries[selectedDate][2]);
+                } else {
+                  setWaterStatus(false);
+                  setRepotStatus(false);
+                  setFertilizeStatus(false);
+                }
+                updateCalendarMarkings();
+              }}
+              onDayLongPress={() => setDisplayAddEntryModal(true)}
+              markedDates={markings}
+              maxDate={new Date()}
+              markingType={'custom'}
+              theme={{
+                backgroundColor: '#ffffff',
+                calendarBackground: '#ffffff',
+                textSectionTitleColor: Colors.grey600,
+                monthTextColor: Colors.grey600,
+                selectedDayTextColor: 'white',
+                selectedDayBackgroundColor: Colors.lightGreen300,
+                arrowColor: Colors.lightGreen300,
+                todayTextColor: Colors.lightGreen300,
+                dayTextColor: Colors.grey600,
+                textDisabledColor: Colors.grey400,
+              }}
+            />
+            {/*<Button onPress={() => setDisplayAddEntryModal(true)}>Edit Entry</Button>*/}
+            <AddEntryModal
+              selectedDate={selectedDate}
+              displayModal={displayAddEntryModal}
+              onPress={() => setDisplayAddEntryModal(false)}
+              onExit={() => setDisplayAddEntryModal(false)}
+              entries={entries}
+              updateCalendarMarkings={() => updateCalendarMarkings}
+            />
+            <SetWaterReminderModal
+              displayModal={displayWaterModal}
+              onExit={() => setDisplayWaterModal(false)}
+            />
+            <SetRepotReminderModal
+              displayModal={displayRepotModal}
+              onExit={() => setDisplayRepotModal(false)}
+            />
+            <SetFertilizeReminderModal
+              displayModal={displayFertilizeModal}
+              onExit={() => setDisplayFertilizeModal(false)}
+            />
+          </View>
         </View>
       </ScrollView>
     </View>
@@ -396,17 +405,43 @@ const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    //paddingLeft: 20,
     flex: 1,
     backgroundColor: '#fff',
   },
 
+  names: {
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: windowWidth * 0.51,
+    marginLeft: 10
+  },
+
+  contentContainer: {
+    flexDirection: 'column',
+    width: windowWidth * 0.95,
+    alignItems: 'flex-start',
+    paddingLeft: 20
+  },
+
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    // borderWidth: 1, 
+    // borderStyle: "solid",
+    width: windowWidth * 0.85,
+    height: windowHeight * 0.047,
+  },
+  paragraphContent: {
+    paddingLeft: 10
+  },
+
   buttonStyle: {
     borderRadius: 1,
-    fontSize: 18,
-    textTransform: 'none',
-    color: '#64A3A3',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
   },
 
   columnStyle: {
@@ -433,7 +468,9 @@ const styles = StyleSheet.create({
 
   profilePicture: {
     flexDirection: 'column',
-    borderColor: 'black',
+    borderColor: Colors.grey400,
+    top: '10%',
+    borderWidth: 2,
     width: 150,
     height: 150,
     borderRadius: 100,
@@ -441,40 +478,23 @@ const styles = StyleSheet.create({
 
   NRTChildStyle: {
     alignSelf: 'center',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    color: '#666666',
-    fontSize: 16,
     lineHeight: 30,
   },
 
   NRTParentStyle: {
     marginLeft: 15,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    color: '#000',
-    fontSize: 22,
     lineHeight: 30,
     justifyContent: 'space-around',
   },
   plantNameStyle: {
     alignContent: 'center',
     top: '30%',
-    fontSize: 26,
     marginLeft: 10,
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    color: '#000',
     paddingBottom: 3,
   },
 
   randomStyling: {
-    fontStyle: 'normal',
-    fontWeight: 'normal',
-    color: '#666666',
-    fontSize: 16,
-    lineHeight: 30,
-    marginLeft: 40,
+    marginLeft: 30,
   },
 
   scientificZoneStyle: {
@@ -493,8 +513,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: 'space-between',
     alignSelf: 'center',
-   // borderWidth: 1, 
-   // borderStyle: "solid",
+    // borderWidth: 1, 
+    // borderStyle: "solid",
     width: windowWidth * 0.78,
     height: windowHeight * 0.047,
   },
