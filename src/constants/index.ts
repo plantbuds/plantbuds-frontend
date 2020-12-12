d00c3afe87bdd66b7b04a3a8516343b6b5bfef53
Justin Sung <47491122+jsungg@users.noreply.github.com>
2020-12-12 15:56:26 -0800

    Revert "cleaning up index file (#56)" (#57)
    This reverts commit 34dbcb4e4d9388a9d27f148f4d955653d40de539.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
-export const API_ROOT = "http://54.183.190.186:8000";
+//export const API_ROOT = "http://54.183.190.186:8000";
+export const API_ROOT = 'http://20b944b88fa4.ngrok.io'; // for testing
 export const BASIC_TOKEN = 'eXdlOkRSeWtnWjM3RHdkRVk3Y3E=';
