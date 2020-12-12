137f53c00fb2122c949d22ec0c6f626b2c0a4e64
jsungg <justinsungg@gmail.com>
2020-12-12 15:58:08 -0800

    removed package lock and nodue modules

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
-//export const API_ROOT = "http://54.183.190.186:8000";
-export const API_ROOT = 'http://20b944b88fa4.ngrok.io'; // for testing
+export const API_ROOT = "http://54.183.190.186:8000";
 export const BASIC_TOKEN = 'eXdlOkRSeWtnWjM3RHdkRVk3Y3E=';
